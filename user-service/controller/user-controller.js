eec9e28ad5ff5160b21e948a82b99881c5af40d4
Moley456 <kingstarwei10@gmail.com>
2022-09-06 02:17:40 +0800

    implement features to fulfill FR 1.2 and FR 1.3

--- user-service/controller/user-controller.js
+++ user-service/controller/user-controller.js
@@ -1,4 +1,8 @@
-import { ormCreateUser as _createUser } from '../model/user-orm.js'
+import {
+    ormCreateUser as _createUser,
+    ormFindUser as _findUser,
+} from '../model/user-orm.js';
+import jwt from 'jsonwebtoken';
 
 export async function createUser(req, res) {
     try {
@@ -7,15 +11,55 @@ export async function createUser(req, res) {
             const resp = await _createUser(username, password);
             console.log(resp);
             if (resp.err) {
-                return res.status(400).json({message: 'Could not create a new user!'});
+                return res.status(400).json({ message: 'Could not create a new user!' });
+            } else if (!resp) {
+                console.log(
+                    `Failed to create new user. user ${username} already exists.`
+                );
+                return res.status(409).json({
+                    message:
+                        'Username already exists! Please try again with a different username.',
+                });
             } else {
-                console.log(`Created new user ${username} successfully!`)
-                return res.status(201).json({message: `Created new user ${username} successfully!`});
+                console.log(`Created new user ${username} successfully!`);
+                return res
+                    .status(201)
+                    .json({ message: `Created new user ${username} successfully!` });
             }
         } else {
-            return res.status(400).json({message: 'Username and/or Password are missing!'});
+            return res
+                .status(400)
+                .json({ message: 'Username and/or Password are missing!' });
         }
     } catch (err) {
-        return res.status(500).json({message: 'Database failure when creating new user!'})
+        return res
+            .status(500)
+            .json({ message: 'Database failure when creating new user!' });
+    }
+}
+
+export async function login(req, res) {
+    try {
+        const { username, password } = req.body;
+        if (!(username && password)) {
+            return res
+                .status(400)
+                .json({ message: 'Username and/or Password are missing!' });
+        }
+
+        const user = await _findUser(username);
+
+        if (user && user.password == password) {
+            const token = jwt.sign(
+                { user_id: user._id, username },
+                process.env.JWT_TOKEN_KEY
+            );
+
+            return res.status(200).json({ token: token });
+        }
+
+        return res.status(400).json({ message: 'Invalid Credentials!' });
+    } catch (err) {
+        return res.status(400).json({ message: 'Login failed!' });
     }
 }
--- user-service/model/repository.js
+++ user-service/model/repository.js
@@ -1,17 +1,25 @@
 import UserModel from './user-model.js';
-import 'dotenv/config'
+import 'dotenv/config';
 
 //Set up mongoose connection
 import mongoose from 'mongoose';
 
-let mongoDB = process.env.ENV == "PROD" ? process.env.DB_CLOUD_URI : process.env.DB_LOCAL_URI;
+let mongoDB =
+    process.env.ENV == 'PROD' ? process.env.DB_CLOUD_URI : process.env.DB_LOCAL_URI;
 
-mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-export async function createUser(params) { 
-  return new UserModel(params)
+export async function createUser(params) {
+    return new UserModel(params);
 }
 
+export async function exists(userID) {
+    return await UserModel.exists({ username: userID });
+}
+
+export async function findUser(userID) {
+    return await UserModel.findOne({ username: userID });
+}
