f67d9225629edb70c9574aeb08fbaf7182a4f197
sin yee <e0563953@u.nus.edu>
2022-09-20 14:49:12 +0800

    emit 'match' event every second and 'matchCancel' event when user cancels the match

--- frontend/src/components/MatchingPage.js
+++ frontend/src/components/MatchingPage.js
@@ -19,23 +19,23 @@ import {CountdownCircleTimer} from "react-countdown-circle-timer";
 
 var PORT = 8001;
 
-
 function MatchingPage() {
   const [isMatchFail, setIsMatchFail] = useState(false);
   const [isMatchSuccess, setIsMatchSuccess] = useState(false);
   const [isPlaying, setIsPlaying] = useState(true);
   const [key, setKey] = useState(0);
+  const [fireMatch, setFireMatch] = useState(0);
   const navigate = useNavigate();
   const socket = io.connect(`http://localhost:${PORT}`) 
 
   useEffect(() => {
-    socket.emit('match', `${key}`);
-  }, [key]);
+    console.log(`${fireMatch}`, 'match event sent')
+    socket.emit('match', `username`);
+  }, [key, fireMatch]);
 
   useEffect(() => {
     socket.on('matchFail', () => {
       console.log('matchfail')
-      // setIsMatchFail(true);
     });
     socket.on('matchSuccess', () => {
       setIsMatchSuccess(true);
@@ -58,15 +58,14 @@ function MatchingPage() {
     );
   };
 
-  
   const handleRetry = () => {
     setIsMatchFail(false);
     setKey(prevKey => prevKey + 1);
   };
 
   const handleBack = () => {
-    // socket.disconnect();
-    navigate();
+    socket.emit('matchCancel', `username`);
+    navigate('/dashboard');
   };
 
   return (
@@ -85,14 +84,15 @@ function MatchingPage() {
           width:'100%',
           marginBottom:"3rem"
         }}>
-          {!isMatchFail?
-          <Typography variant={"h4"}>
-            Matching in progress...
-          </Typography>
-          :
-          <Typography variant={"h4"}>
-            No match found :(
-          </Typography>
+          {!isMatchFail
+            ?
+            <Typography variant={"h4"}>
+              Matching in progress...
+            </Typography>
+            :
+            <Typography variant={"h4"}>
+              No match found :(
+            </Typography>
           }
         </Box>
         <CountdownCircleTimer
@@ -104,6 +104,7 @@ function MatchingPage() {
           colorsTime={[30, 20, 10, 0]}
           trailColor="#d6d6d6"
           onComplete={() => setIsMatchFail(true)}
+          onUpdate={() => setFireMatch(fireMatch => fireMatch + 1)}
         >
           {renderTime}
         </CountdownCircleTimer>
@@ -114,24 +115,23 @@ function MatchingPage() {
           justifyContent="center"
           sx={{marginTop: "2rem"}}
         > 
-        {isMatchFail &&
           <Zoom
             in={true}
             sx={{ transitionDelay: '1500ms' }}
           >
             <Grid container item xs={3} justifyContent="center">
-              <Button variant={"outlined"} onClick={handleBack} color={"secondary"}>Back</Button>
+              <Button variant={"outlined"} onClick={handleBack} color={"secondary"}>Cancel match</Button>
             </Grid>
-          </Zoom>}
-        {isMatchFail && 
-          <Zoom
-            in={true}
-            sx={{ transitionDelay: '1500ms' }}
-          >
-            <Grid container item xs={3} justifyContent="center">
-              <Button variant={"outlined"} onClick={handleRetry} color={"secondary"}>Retry</Button>
-            </Grid>
-          </Zoom>}
+          </Zoom>
+          {isMatchFail && 
+            <Zoom
+              in={true}
+              sx={{ transitionDelay: '1500ms' }}
+            >
+              <Grid container item xs={3} justifyContent="center">
+                <Button variant={"outlined"} onClick={handleRetry} color={"secondary"}>Retry</Button>
+              </Grid>
+            </Zoom>}
         </Grid>
       </Box>
   );
