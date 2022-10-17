04bfc61f12e830d9056a6e9b97dfd2bc2f9714e0
sin yee <e0563953@u.nus.edu>
2022-10-17 21:20:06 +0800

    set up video service

---
+++ video-service/controller/video-controller.js
@@ -0,0 +1,29 @@
+import pkg from 'agora-access-token';
+const { RtcTokenBuilder, RtcRole } = pkg;
+import 'dotenv/config';
+
+const AGORA_APP_ID = process.env.AGORA_APP_ID;
+const AGORA_APP_CERTIFICATE = process.env.AGORA_APP_CERTIFICATE;
+
+export const generateAccessToken = (req, res) => {
+    const { channel } = req.body;
+    if (!channel) {
+        return res.status(500).json({message:'Channel is missing!'});
+    }
+
+    const role = RtcRole.PUBLISHER;
+    const uid = Math.floor(Math.random() * 100000);
+    const expirationTimeInSeconds = 3600;
+    const currentTimestamp = Math.floor(Date.now() / 1000);
+    const privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds;
+    
+    const token = RtcTokenBuilder.buildTokenWithUid(
+        AGORA_APP_ID,
+        AGORA_APP_CERTIFICATE,
+        channel,
+        uid,
+        role,
+        privilegeExpiredTs,
+    );
+    return res.json({ token });
+}
