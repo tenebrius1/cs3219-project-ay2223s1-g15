bcfd293d8af14fd3e1b0ac9c3a434dfd2fff04f6
sin yee <e0563953@u.nus.edu>
2022-10-17 21:20:45 +0800

    Add frontend logic for video calling

--- frontend/src/configs.js
+++ frontend/src/configs.js
@@ -2,4 +2,9 @@ const URI_USER_SVC = process.env.URI_USER_SVC || 'http://localhost:8000'
 
 const PREFIX_USER_SVC = '/api/user'
 
+const URI_VIDEO_SVC = process.env.URI_VIDEO_SVC || 'http://localhost:8003'
+
+const PREFIX_VIDEO_SVC = '/rtctoken'
+
 export const URL_USER_SVC = URI_USER_SVC + PREFIX_USER_SVC
+export const URL_VIDEO_SVC = URI_VIDEO_SVC + PREFIX_VIDEO_SVC
