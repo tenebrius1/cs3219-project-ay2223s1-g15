df085cdfdc92f640ae8c9fdb0b6f6ce17c180402
karhee <eekarheenicholas@gmail.com>
2022-11-06 03:53:36 +0800

    chore: remove morgan to prevent unnecessary logging

--- matching-service/index.js
+++ matching-service/index.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
-import { createServer } from 'http';
-import { startServer } from './socket/socket-server.js';
+import express from "express";
+import cors from "cors";
+// import morgan from 'morgan';
+import { createServer } from "http";
+import { startServer } from "./socket/socket-server.js";
 
 const app = express();
 var PORT = process.env.PORT || 8001;
@@ -10,12 +10,12 @@ var PORT = process.env.PORT || 8001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors()); // config cors so that front-end can use
-app.options('*', cors());
+app.options("*", cors());
 
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
-app.get('/matching', (_, res) => {
-  res.send('Hello World from matching-service');
+app.get("/matching", (_, res) => {
+  res.send("Hello World from matching-service");
 });
 
 export const httpServer = createServer(app);
